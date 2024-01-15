c9704c6045660cd5311f5c037bb00d4f19b416c0
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2024-01-15 23:40:00 +0200

    course assignment portal

---
+++ scripts/getDates.js
@@ -0,0 +1,4 @@
+//copyright year
+document.querySelector('#copyrightYear').innerText = new Date().getFullYear();
+//last modified
+document.querySelector('#lastModified').innerText = 'last Modified: ' + document.lastModified;
